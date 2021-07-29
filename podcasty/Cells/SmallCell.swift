39cdd3fbe253ac7a5f9c014d0a3de31cb2246d83
gurrium <gurrium@hatena.ne.jp>
2021-07-29 20:23:00 +0900

    SmallCellを作った

---
+++ podcasty/Cells/SmallCell.swift
@@ -0,0 +1,17 @@
+//
+//  SmallCell.swift
+//  podcasty
+//
+//  Created by gurrium on 2021/07/29.
+//
+
+import UIKit
+
+class SmallCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
